e064fcd8de6bc02f647db62a3e8620be2de14670
Chris Yee <kasinyee@outlook.com>
2024-03-29 11:36:33 -0400

    reload files

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
-resource "google_container_registry_repository" "docker_repo" {
-  name = "my-docker-repo"
+resource "google_project_iam_member" "artifact_registry_service_account" {
+  project = "my-second-project-418213"
+  role    = "roles/artifactregistry.serviceAgent"
+  member  = "ksyservacc@my-second-project-418213.iam.gserviceaccount.com"
+}
+
+resource "google_service_account" "artifact_registry" {
+  account_id   = "artifact-registry-sa"
+  display_name = "Artifact Registry Service Account"
+}
+
+resource "google_artifact_registry_repository" "docker_repo" {
+  provider      = google-beta
+  project       = "my-second-project-418213"
+  location      = "US"
+  repository_id = "my-docker-repo"
+  format        = "DOCKER"
 }
