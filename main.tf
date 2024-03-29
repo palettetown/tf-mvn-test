049a6b47ad405e46dd36a0f808072132076a41ce
Chris Yee <kasinyee@outlook.com>
2024-03-29 15:00:18 -0400

    updated

--- main.tf
+++ main.tf
@@ -9,9 +9,9 @@ terraform {
 
 provider "google" {
   # Configuration options
-  project     = "my-second-project-418213"
-  region      = "northamerica-northeast2"
-  credentials = file("C:\\MyPrograms\\GCP\\my-second-project-418213-c4584d61b2a8.json")
+  project = "my-second-project-418213"
+  region  = "northamerica-northeast2"
+  #credentials = file("C:\\MyPrograms\\GCP\\my-second-project-418213-c4584d61b2a8.json")
 }
 
 # Task 2: Create Docker repository in GCP Artifact Registry
