1f89aa75e78e3d8cfdf7eb023282dfca986fd0b4
palettetown <128302739+palettetown@users.noreply.github.com>
2024-03-29 12:17:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-#resource "google_project_iam_member" "artifact_registry_service_account" {
-#  project = "my-second-project-418213"
-#  role    = "roles/artifactregistry.serviceAgent"
-#  member  = "serviceAccount:ksyservacc@my-second-project-418213.iam.gserviceaccount.com"
-#}
 terraform {
   required_providers {
     google = {
@@ -19,11 +14,6 @@ provider "google" {
   #credentials = file("C:\\MyPrograms\\GCP\\my-second-project-418213-c4584d61b2a8.json")
 }
 
-#resource "google_service_account" "artifact_registry" {
-#  account_id   = "artifact-registry-sa"
-#  display_name = "Artifact Registry Service Account"
-#}
-
 # Task 2: Create Docker repository in GCP Artifact Registry
 resource "google_artifact_registry_repository" "docker_repo" {
   provider      = google
@@ -32,14 +22,3 @@ resource "google_artifact_registry_repository" "docker_repo" {
   repository_id = "my-docker-repo"
   format        = "DOCKER"
 }
-
-# Task 3: Create Docker image and upload to the Docker repository
-#resource "google_cloudbuild_trigger" "build_trigger" {
-#  name        = "docker-image-build-trigger"
-#  description = "Trigger to build and push Docker image to Artifact Registry"
-#  trigger_template {
-#    repo_name   = google_artifact_registry_repository.docker_repo.name
-#    branch_name = "main"
-#  }
-#  filename = "cloudbuild.yaml"
-#}
