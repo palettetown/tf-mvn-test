3ab83ff23fb68edbc7513bf68d3fb8b56ac91e8d
Chris Yee <kasinyee@outlook.com>
2024-03-29 11:48:21 -0400

    reload files

--- main.tf
+++ main.tf
@@ -3,6 +3,20 @@
 #  role    = "roles/artifactregistry.serviceAgent"
 #  member  = "serviceAccount:ksyservacc@my-second-project-418213.iam.gserviceaccount.com"
 #}
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.21.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "my-second-project-418213"
+  region = "northamerica-northeast2"
+}
 
 resource "google_service_account" "artifact_registry" {
   account_id   = "artifact-registry-sa"
@@ -10,9 +24,8 @@ resource "google_service_account" "artifact_registry" {
 }
 
 resource "google_artifact_registry_repository" "docker_repo" {
-  provider      = google-beta
-  project       = "my-second-project-418213"
-  location      = "US"
+  provider      = google
+  location      = "NORTHAMERICA-NORTHEAST2"
   repository_id = "my-docker-repo"
   format        = "DOCKER"
 }
