7d67ef1c73397c56eaecf2d9ecb4ddfc52610224
palettetown <128302739+palettetown@users.noreply.github.com>
2024-03-31 17:16:17 -0400

    Update HelloWorld.java

--- src/main/java/com/HelloWorld.java
+++ src/main/java/com/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Welcome to our application amigo");
+        System.out.println("Welcome to Canada!!");
     }
 }
