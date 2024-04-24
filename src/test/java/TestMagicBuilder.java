77655f74d27daca9d82d9c3af3ecad76f3d8cc89
palettetown <128302739+palettetown@users.noreply.github.com>
2024-04-24 14:11:03 -0400

    Update TestMagicBuilder.java

--- src/test/java/TestMagicBuilder.java
+++ src/test/java/TestMagicBuilder.java
@@ -6,7 +6,7 @@ public class TestMagicBuilder {
 
     @Test
     public void testLucky() {
-        assertEquals(7, MagicBuilder.getLucky());
+        assertEquals(8, MagicBuilder.getLucky());
     }
 
 }
